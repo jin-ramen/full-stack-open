9c2968c1e78801dcdddb4f4ac27d8152f9403b5c
Jin <jinhong36@icloud.com>
2026-01-07 17:42:12 +1100

    added fethcing using effect hook

--- part_2/phonebook/src/App.jsx
+++ part_2/phonebook/src/App.jsx
@@ -1,18 +1,24 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/Filter'
 import PersonForm from './components/PersonForm'
 import Persons from './components/Persons'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { 
-      name: 'Arto Hellas' , 
-      number: '040-123456'
-    }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewname] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newSearch, setNewSearch] = useState('')
+
+  useEffect(() => {
+    console.log('effect')
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        console.log('promise fulfilled')
+        setPersons(response.data)
+      })
+  }, [])
 
   const addPerson = (event) => {
     event.preventDefault()
