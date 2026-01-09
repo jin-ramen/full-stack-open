cad90b55a2f01b3cfd154483ac04089d5643c37d
Jin <jinhong36@icloud.com>
2026-01-09 19:37:46 +1100

    Add initial HTML and SVG assets for phonebook application

--- part_3/index.js
+++ part_3/index.js
@@ -4,6 +4,7 @@ const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('body', (req) => JSON.stringify(req.body))
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
@@ -91,7 +92,7 @@ app.post('/api/persons', (request, response) => {
   }
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
