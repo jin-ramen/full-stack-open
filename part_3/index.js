cf7755aae46191fdacd321a6cd46a080ca3ce9c5
Jin Hong Pang <jinhong36@icloud.com>
2026-01-09 15:13:31 +1100

    feat: implement CRUD operations for persons in the phonebook API

--- part_3/index.js
+++ part_3/index.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const app = express()
 
+app.use(express.json())
+
 let persons = [
     { 
       "id": "1",
@@ -35,6 +37,53 @@ app.get('/info', (request, response) => {
         <p>${new Date()}</p>
         `
     )
+})
+
+app.get('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  const person = persons.find(person => person.id === id)
+
+  if (person) {
+    response.json(person)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+  const id = request.params.id
+  persons = persons.filter(person => person.id !== id)
+  response.status(204).end()
+})
+
+const generateId = () => String(Math.floor(Math.random() * 100))
+
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+  console.log(body)
+
+  if (!body.name) {
+    response.status(400).json({
+      error: 'name is missing'
+    })
+  } else if (!body.number) {
+    response.status(400).json({
+      error: 'number is missing'
+    })
+  } else if (persons.some(person => person.name.toLowerCase() === body.name.toLowerCase())){
+    response.status(400).json({
+      error: 'name must be unique'
+    })
+  } else {
+    const person = {
+      id: generateId(),
+      name: body.name,
+      number: body.number
+    }
+
+    persons = persons.concat(person)
+    response.json(person)
+  }
 })
 
 const PORT = 3001
